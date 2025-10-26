3f59f3fe4ab75a98b42aa3dc26a733c9f9c967dd
Micha <micha@argv.nl>
2025-10-26 17:19:03 +0100

    parse url, lookup dns

--- README.md
+++ README.md
@@ -10,15 +10,15 @@ A command-line tool for diagnosing connection issues and analyzing a myriad of H
 paru -Syu durl
 ```
 
-### other distros
+### other distros (release bin)
 
 ```shell
 sudo wget https://github.com/FurriousFox/durl/releases/latest/download/durl_linux_$(uname -p)_musl -O /usr/bin/durl
 sudo chmod +x /usr/bin/durl
 ```
 
-## building
+### building from source
 
 ```shell
-git clone https://github.com/FurriousFox/durl.git
+go install argv.nl/durl@latest
 ```
--- main.go
+++ main.go
@@ -2,9 +2,69 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"net"
+	"net/url"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello world!", rand.Intn(123))
+	if len(os.Args[1:]) != 1 {
+		fmt.Fprintln(os.Stderr, "use 'durl <url>'")
+		return
+	}
+
+	// parse url
+	var url, error = url.Parse(os.Args[1])
+	if error != nil {
+		var url2, error2 = url.Parse("https://" + os.Args[1])
+		if error2 == nil {
+			url = url2
+		} else {
+			fmt.Fprintln(os.Stderr, "idk error", error)
+			return
+		}
+	}
+
+	if url.Scheme == "" {
+		var url2, error2 = url.Parse("https://" + os.Args[1])
+		if error2 == nil {
+			url = url2
+		} else {
+			fmt.Fprintln(os.Stderr, "idk error", error)
+			return
+		}
+	} else if url.Scheme != "https" {
+		fmt.Fprintf(os.Stderr, "unsupported protocol '%s'\n", url.Scheme)
+		return
+	}
+
+	if url.Hostname() == "" {
+		fmt.Fprintln(os.Stderr, "hostname required")
+		return
+	}
+
+	fmt.Println(url)
+	fmt.Println(url.Hostname())
+
+	var hostname = url.Hostname()
+
+	var ips, dns_error = net.LookupIP(hostname)
+	if dns_error != nil {
+		fmt.Fprintln(os.Stderr, "idk error", error)
+		return
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
+	// try tcp
+
+	// try tls 1.*
+
+	// try http 1
+	// try http 2
+	// try http 3
+
+	// certificate info
 }
